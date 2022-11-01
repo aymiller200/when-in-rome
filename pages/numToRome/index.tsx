20c95cba982d9da84af1013ee4e32cfa148081dd
aymiller200 <aymiller200@gmail.com>
2022-11-01 17:20:46 -0400

    Removed file builder functionality

--- pages/numToRome/index.tsx
+++ pages/numToRome/index.tsx
@@ -1,59 +1,31 @@
-import Error from '../_error'
+
 import Link from 'next/link'
 import Layout from '../components/layout'
 import Title from '../components/title'
 import Navigation from '../components/nav'
 import Footer from '../components/footer'
+import { romeObj } from '../../helpers/romanNumerals'
 
-import { useState, ReactElement } from 'react'
+import React, { useState, ReactElement } from 'react'
 
 import conversionStyle from '../../styles/Conversion.module.css'
-
-interface NumToRoman {
-  entry: string
-  romanEntry: string
-}
-
-interface Entry {
-  entry: NumToRoman
-  message: string
-}
 
 const NumToRoman = () => {
   const [entry, setEntry] = useState('')
   const [entryNumRes, setEntryNumRes] = useState('')
-  const [statusCode, setStatusCode] = useState('')
-  const [statusMessage, setStatusMessage] = useState('')
-  const [error, setError] = useState(false)
 
-  function formHandler(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    fetch('/api/conversionToRoman', {
-      method: 'POST',
-      body: JSON.stringify({
-        entry: entry
-      }),
-      headers: {
-        'Content-type': 'application/json',
-      },
+  function convertToRoman(num: number, e?: React.FormEvent) {
+    e?.preventDefault()
+    let romanNum = ''
+    const romanKeys = Object.keys(romeObj).reverse()
+    romanKeys.forEach(key => {
+      while (num >= romeObj[key]) {
+        romanNum += key
+        num -= romeObj[key]
+      }
     })
-
-      .then((res) => {
-        if (!res.ok) {
-          setError(true)
-          setStatusCode(res.status.toString())
-          return res.json()
-        }
-        setError(false)
-        return res.json()
-      })
-      .then((data: Entry) => {
-        if (!data.entry) {
-          setStatusMessage(data.message)
-        }
-        setEntryNumRes(data.entry.romanEntry)
-      })
+   
+    setEntryNumRes(romanNum)
   }
 
   function entryHandler(e: React.FormEvent<HTMLInputElement>) {
@@ -65,12 +37,12 @@ const NumToRoman = () => {
     <>
     <div className={conversionStyle.formContainer}>
       
-      <form onSubmit={formHandler} className={conversionStyle.form}>
+      <form className={conversionStyle.form}>
         <div >
           <label htmlFor="entry" className={conversionStyle.title}>Enter a Number to Get a Roman Numeral</label>
           <input type="text" id="entry" value={entry} onChange={entryHandler} className={conversionStyle.input} />
         </div>
-        <button className={conversionStyle.btn}>Convert!</button>
+        <button onClick={(event: React.FormEvent) => convertToRoman(Number(entry), event)} className={conversionStyle.btn}>Convert!</button>
       </form>
 
       <h1 className={conversionStyle.result}>{entryNumRes}</h1>
@@ -78,7 +50,7 @@ const NumToRoman = () => {
       <div className={conversionStyle.link}>
         <Link href='/romeToNum'><a className={conversionStyle.switch}> {`-> `} Convert Roman Numeral to a Number {` <-`} </a></Link>
       </div>
-      {error ? <Error statusCode={statusCode} statusMessage={statusMessage} /> : null}
+ 
     </div>
     
     </>
--- pages/romeToNum/index.tsx
+++ pages/romeToNum/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ReactElement } from 'react'
+import React, { useState, ReactElement} from 'react'
 import Link from 'next/link'
 import Error from '../_error'
 import Layout from '../components/layout'
@@ -6,16 +6,8 @@ import Title from '../components/title'
 import Navigation from '../components/nav'
 import Footer from '../components/footer'
 import conversionStyle from '../../styles/Conversion.module.css'
+import { romeObj } from "../../helpers/romanNumerals";
 
-interface RomeToNum {
-  numResult: string
-  romanEntry: string
-}
-
-interface Entry {
-  entry: RomeToNum
-  message: string
-}
 
 const RomanToNum = () => {
   const [romeEntry, setRomeEntry] = useState('')
@@ -24,40 +16,38 @@ const RomanToNum = () => {
   const [statusMessage, setStatusMessage] = useState('')
   const [error, setError] = useState(false)
 
-  function formHandler(event: React.FormEvent<HTMLFormElement>) {
+  function convertToNumber(str: string, event: React.FormEvent) {
     event.preventDefault()
+    const pattern = /^M{0,1}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+    let num = 0;
+    let romanNum = ''
 
-    fetch('/api/conversionToNum', {
-      method: 'POST',
-      body: JSON.stringify({
-        romeEntry: romeEntry.toUpperCase()
-      }),
-      headers: {
-        'Content-type': 'application/json'
+    str.split('').forEach((val, i) => {
+      //str ["X", "X", "X", "V", "I"]
+      //val [10, 10, 10, 5, 1] 
+      // 0     1    2    3    4  
+      if (romeObj[val] < romeObj[str[i + 1]]) {
+        num -= romeObj[val] // 1 - 0
+      } else {
+        num += romeObj[val]
       }
+      romanNum = num.toString()
     })
-      .then((res) => {
-        if (!res.ok) {
-          setError(true)
-          setStatusCode(res.status.toString())
 
-          return res.json()
-        }
-        setError(false)
-        return res.json()
+    if(!str.match(pattern)){
+      setEntryRomeRes('')
+      setError(true)
+      setStatusMessage('Ope! Remember, Roman Numerals are symbols like I, IV, X!')
+      setStatusCode(String(404))
+    }else{
+      setEntryRomeRes(romanNum)
+      setError(false)
+      setStatusMessage('')
+      setStatusCode('')
+    }
 
-      })
-      .then((data: Entry) => {
-
-        if (!data.entry) {
-          setStatusMessage(data.message)
-        }
-
-        setEntryRomeRes(data.entry.numResult)
-
-      })
   }
-
+  
   function entryHandler(e: React.FormEvent<HTMLInputElement>) {
     const enteredRomeNumeral = (e.currentTarget.value)
     setRomeEntry(enteredRomeNumeral)
@@ -66,7 +56,7 @@ const RomanToNum = () => {
   return (
     <>
       <div className={conversionStyle.formContainer}>
-        <form onSubmit={formHandler} className={conversionStyle.form}>
+        <form onSubmit={(event: React.FormEvent) => convertToNumber(romeEntry, event)} className={conversionStyle.form}>
           <div>
             <label htmlFor="romanEntry" className={conversionStyle.title}>Enter a Roman Numeral to Get a Number</label>
 
